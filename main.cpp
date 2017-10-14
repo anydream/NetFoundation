de737b6645a1e1b113a486767cab4107f11a5c15
anydream <anydreamplay@hotmail.com>
2017-10-15 00:29:57 +0800

    ~

--- main.cpp
+++ main.cpp
@@ -2,217 +2,221 @@
 #include <vld.h>
 #endif
 
-#include <stdio.h>
-#include "uvpp/uvpp.h"
-#include "NFTimer.h"
-#include "common/SharedUniquePtr.h"
-#include <string>
 #include <assert.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <malloc.h>
+#include <string>
+#include "DateTime.h"
+#include "StringConvert.h"
+#include "uvpp.h"
+#include "common/SharedUniquePtr.h"
+
+#pragma execution_character_set("utf-8")
+
+class Logger
+{
+public:
+	~Logger()
+	{
+		fclose(FilePtr_);
+	}
+
+	void Init(const char *fname)
+	{
+		assert(!FilePtr_);
+		FilePtr_ = fopen(fname, "ab");
+	}
+
+	void LogBuffer(size_t len, const char *data) const
+	{
+		fwrite(data, 1, len, FilePtr_);
+	}
+
+	void Log(const char *fmt, ...)
+	{
+		va_list args;
+		va_start(args, fmt);
+
+		int bufSize = _vscprintf(fmt, args) + 1;
+		char *str = static_cast<char*>(alloca(bufSize));
+		bufSize = vsnprintf(str, bufSize, fmt, args);
+
+		va_end(args);
+
+		std::string strTimestamp = "[" + RCSoft::Utils::DateTime::Now().ToString("%Y-%m-%d %H:%M:%S") + "] ";
+		LogBuffer(strTimestamp.size(), strTimestamp.data());
+		LogBuffer(bufSize, str);
+
+		IsDirty_ = true;
+	}
+
+	template <class ...TArgs>
+	void operator () (const char *fmt, TArgs &&...args)
+	{
+		Log(fmt, std::forward<TArgs>(args)...);
+	}
+
+	void TryFlush()
+	{
+		if (IsDirty_)
+		{
+			IsDirty_ = false;
+			fflush(FilePtr_);
+		}
+	}
+
+private:
+	FILE *FilePtr_ = nullptr;
+	bool IsDirty_ = false;
+};
+
+static Logger Log;
+using namespace uvpp;
 
 #define KEY_STATE(_vk) (GetAsyncKeyState(_vk) & 0x8000)
 
-int printf_flush(const char *fmt, ...)
+static std::string ToAddressName(sockaddr_storage *addr)
 {
-	va_list args;
-	va_start(args, fmt);
-
-	int bufSize = _vscprintf(fmt, args) + 1;
-	char *str = static_cast<char*>(alloca(bufSize));
-	bufSize = vsnprintf(str, bufSize, fmt, args);
-
-	va_end(args);
-
-	fwrite(str, 1, bufSize, stdout);
-	fflush(stdout);
-
-	return bufSize;
+	assert(addr->ss_family == AF_INET);
+	int port;
+	std::string result = UvMisc::ToNameIPv4(reinterpret_cast<sockaddr_in*>(addr), &port);
+	result.push_back(':');
+	return result + std::to_string(port);
 }
 
-//////////////////////////////////////////////////////////////////////////
-namespace TestUvpp
+static void ServerLoop(int port)
 {
-	using namespace uvpp;
+	UvLoop loop;
 
-	static void TestTimer(UvLoop &loop)
+	// 创建服务端
+	UvTCP *pServer = new UvTCP;
+	int status = pServer->Init(loop);
+	Log("* 初始化: %d\n", status);
+
+	// 绑定端口
+	sockaddr_in addr;
+	UvMisc::ToAddrIPv4("0.0.0.0", port, &addr);
+	status = pServer->Bind(reinterpret_cast<const sockaddr*>(&addr));
+	Log("* 绑定: %d\n", status);
+
+	// 开始监听
+	status = pServer->Listen([&loop,
+		numClients = SharedUniquePtr<int>(new int(0))](UvStream *server, int status)
 	{
-		UvTimer *pTimer = new UvTimer();
-
-		int status = pTimer->Init(loop);
-		printf_flush("* Timer.Init: %d\n", status);
-
-		status = pTimer->Start(
-			500, 1000,
-			[&loop,
-			pTimer,
-			pCounter = SharedUniquePtr<int>(new int(0))]()
+		if (status != 0)
 		{
-			printf_flush("* Timeout ID: %d\n", *pCounter);
-			if (++*pCounter == 5)
-			{
-				printf_flush("* Timer.Finished\n");
-				loop.DelayDelete(pTimer);
-			}
-		});
+			Log("* 侦听失败: %d, %s\n", status, UvMisc::ToError(status));
+			return;
+		}
 
-		printf_flush("* Timer.Start: %d\n", status);
-	}
-
-	static std::string ToAddressName(sockaddr_storage *addr)
-	{
-		assert(addr->ss_family == AF_INET);
-		int port;
-		std::string result = UvMisc::ToNameIPv4(reinterpret_cast<sockaddr_in*>(addr), &port);
-		result.push_back(':');
-		return result + std::to_string(port);
-	}
-
-	static void TestTCPServer(UvLoop &loop)
-	{
-		// 创建服务端
-		UvTCP *pServer = new UvTCP;
-		int status = pServer->Init(loop);
-		printf_flush("* 初始化: %d\n", status);
-
-		// 绑定端口
-		sockaddr_in addr;
-		UvMisc::ToAddrIPv4("0.0.0.0", 80, &addr);
-		status = pServer->Bind(reinterpret_cast<const sockaddr*>(&addr));
-		printf_flush("* 绑定: %d\n", status);
-
-		// 开始监听
-		status = pServer->Listen([&loop,
-			numClients = SharedUniquePtr<int>(new int(0))](UvStream *server, int status)
+		// 创建会话
+		UvTCP *pClient = new UvTCP;
+		pClient->Init(loop);
+		status = server->Accept(pClient);
+		if (status == 0)
 		{
-			if (status != 0)
+			++*numClients;
+
+			auto addrs = pClient->GetPeerName();
+			std::string peerName = ToAddressName(&addrs);
+			Log("* 接受连接: [%s], Total: %d\n",
+				peerName.c_str(),
+				*numClients);
+
+			// 开始接收数据
+			UvBuf *pRecvBuf = new UvBuf;
+			pClient->ReadStart([&loop, &numClients, pClient, peerName,
+				recvBuf = SharedUniquePtr<UvBuf, std::function<void(UvBuf*)>>(pRecvBuf, UvBuf::Deleter)]
+				(UvStream *stream, ssize_t nread, UvBuf *buf)
 			{
-				printf_flush("* 侦听失败: %d, %s\n", status, UvMisc::ToError(status));
+				if (nread < 0)
+				{
+					--*numClients;
+					Log("* 断开连接: [%s], Total: %d. %Id, %s\n", peerName.c_str(), *numClients, nread, UvMisc::ToError(static_cast<int>(nread)));
+					// 停止写入
+					pClient->Shutdown([](int) {});
+					loop.DelayDelete(stream);
+					return;
+				}
+				// echo
+				UvBuf bufRange(nread, buf->Data);
+				pClient->Write(&bufRange, 1, [&loop, &numClients, pClient, peerName](int status)
+				{
+					// eof
+					--*numClients;
+					if (status < 0)
+						Log("* 写入失败: [%s], Total: %d. %d, %s\n", peerName.c_str(), *numClients, status, UvMisc::ToError(status));
+					else
+						Log("* 写入成功: [%s], Total: %d\n", peerName.c_str(), *numClients);
+					pClient->ReadStop();
+					loop.DelayDelete(pClient);
+				});
+				Log("* 读取: [%s], %Id\n[", peerName.c_str(), bufRange.Length);
+				Log.LogBuffer(bufRange.Length, bufRange.Data);
+				const char strLogEnd[] = "]\n";
+				Log.LogBuffer(sizeof(strLogEnd) - 1, strLogEnd);
+			}, [pRecvBuf](UvHandle *handle, size_t suggested_size, UvBuf *buf)
+			{
+				pRecvBuf->Alloc(suggested_size);
+				*buf = *pRecvBuf;
+			});
+		}
+		else
+		{
+			auto addrs = pClient->GetPeerName();
+			Log("* 接受连接失败: [%s], %d, %s\n", ToAddressName(&addrs).c_str(), status, UvMisc::ToError(status));
+			loop.DelayDelete(pClient);
+		}
+	});
+	Log("* 开始侦听: %d\n", status);
+
+	//////////////////////////////////////////////////////////////////////////
+	UvTimer *pFlushTmr = new UvTimer;
+	pFlushTmr->Init(loop);
+	pFlushTmr->Start(0, 5000, []()
+	{
+		Log.TryFlush();
+	});
+
+	UvTimer *pStopTmr = new UvTimer;
+	pStopTmr->Init(loop);
+	pStopTmr->Start(0, 100, [&loop]()
+	{
+		if (KEY_STATE(VK_F10))
+		{
+			const size_t bufSize = 16;
+			char buf[bufSize];
+			printf("* Exit? ");
+			fgets(buf, bufSize, stdin);
+			if (buf[0] != 'y')
 				return;
-			}
 
-			// 创建会话
-			UvTCP *pClient = new UvTCP;
-			pClient->Init(loop);
-			status = server->Accept(pClient);
-			if (status == 0)
+			Log("* 退出\n");
+			loop.Walk([&loop](UvHandle *handle)
 			{
-				++*numClients;
-
-				auto addrs = pClient->GetPeerName();
-				std::string peerName = ToAddressName(&addrs);
-				printf_flush("* 接受连接: [%s], Total: %d\n",
-					peerName.c_str(),
-					*numClients);
-
-				// 开始接收数据
-				UvBuf *pRecvBuf = new UvBuf;
-				pClient->ReadStart([&loop, &numClients, pClient, peerName,
-					recvBuf = SharedUniquePtr<UvBuf, std::function<void(UvBuf*)>>(pRecvBuf, UvBuf::Deleter)]
-					(UvStream *stream, ssize_t nread, UvBuf *buf)
-				{
-					if (nread < 0)
-					{
-						--*numClients;
-						printf_flush("* 断开连接: [%s], Total: %d. %Id, %s\n", peerName.c_str(), *numClients, nread, UvMisc::ToError(nread));
-						// 停止写入
-						pClient->Shutdown([](int) {});
-						loop.DelayDelete(stream);
-						return;
-					}
-					// echo
-					UvBuf bufRange(nread, buf->Data);
-					pClient->Write(&bufRange, 1, [&loop, &numClients, pClient, peerName](int status)
-					{
-						// eof
-						--*numClients;
-						if (status < 0)
-							printf_flush("* 写入失败: [%s], Total: %d. %d, %s\n", peerName.c_str(), *numClients, status, UvMisc::ToError(status));
-						else
-							printf_flush("* 写入成功: [%s], Total: %d\n", peerName.c_str(), *numClients);
-						pClient->ReadStop();
-						loop.DelayDelete(pClient);
-					});
-					printf_flush("* 读取: [%s], %Id [%.*s]\n", peerName.c_str(), bufRange.Length, static_cast<int>(bufRange.Length), bufRange.Data);
-				}, [pRecvBuf](UvHandle *handle, size_t suggested_size, UvBuf *buf)
-				{
-					pRecvBuf->Alloc(suggested_size);
-					*buf = *pRecvBuf;
-				});
-			}
-			else
-			{
-				auto addrs = pClient->GetPeerName();
-				printf_flush("* 接受连接失败: [%s], %d, %s\n", ToAddressName(&addrs).c_str(), status, UvMisc::ToError(status));
-				loop.DelayDelete(pClient);
-			}
-		});
-		printf_flush("* 开始侦听: %d\n", status);
-	}
-
-	static void TestEntry()
-	{
-		printf_flush("===== TestUvpp =====\n");
-
-		UvLoop loop;
-
-		TestTCPServer(loop);
-		//TestTimer(loop);
-
-		UvTimer *pStopTmr = new UvTimer;
-		pStopTmr->Init(loop);
-		pStopTmr->Start(0, 100, [&loop]()
-		{
-			if (KEY_STATE(VK_F10))
-			{
-				printf_flush("* 退出\n");
-
-				loop.Walk([&loop](UvHandle *handle)
-				{
-					printf_flush("* Free: %s\n", handle->GetTypeName());
-					loop.DelayDelete(handle);
-				});
-			}
-		});
-		loop.Run();
-	}
-}
-
-namespace TestNF
-{
-	using namespace NetFoundation;
-
-	static void TestTimer(EventEngine &ee)
-	{
-		Timer *pTimer = new Timer(ee);
-		pTimer->Start(
-			500, 1000,
-			[pTmr = SharedUniquePtr<Timer>(pTimer),
-			pCounter = SharedUniquePtr<int>(new int(0))]() mutable
-		{
-			printf_flush("* Timeout ID: %d\n", *pCounter);
-			if (++*pCounter == 5)
-			{
-				printf_flush("* Timer.Finished\n");
-				pTmr = nullptr;
-			}
-		});
-	}
-
-	static void TestEntry()
-	{
-		printf_flush("===== TestNF =====\n");
-
-		EventEngine ee;
-
-		TestTimer(ee);
-
-		ee.RunDefault();
-	}
+				Log("* 释放: %s\n", handle->GetTypeName());
+				loop.DelayDelete(handle);
+			});
+		}
+	});
+	loop.Run();
 }
 
 //////////////////////////////////////////////////////////////////////////
-int main()
+int main(int argc, const char **argv)
 {
-	TestUvpp::TestEntry();
-	//TestNF::TestEntry();
+	const char *strPort = "80";
+	const char *strLogFile = "netlog.txt";
+
+	if (argc >= 2)
+		strPort = argv[1];
+	if (argc >= 3)
+		strLogFile = argv[2];
+
+	Log.Init(strLogFile);
+
+	ServerLoop(RCSoft::Utils::ParseInt(strPort));
+
 	return 0;
 }
